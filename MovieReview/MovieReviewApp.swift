1b8263c80c191e5546b64b2384b34240e5ada814
Hasib <hasib.jhd@gmail.com>
2023-01-11 23:24:48 +0600

    Initial Commit

---
+++ MovieReview/MovieReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReviewApp.swift
+//  MovieReview
+//
+//  Created by S. M. Hasibur Rahman on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
