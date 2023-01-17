86d725ad57b40ec1ee681b8d4e74445091cf468f
Hasib <hasib.jhd@gmail.com>
2023-01-18 01:04:47 +0600

    Model and Coredata manager added

---
+++ MovieReview/Managers/CoreDataManager.swift
@@ -0,0 +1,23 @@
+//
+//  CoreDataManager.swift
+//  MovieReview
+//
+//  Created by S. M. Hasibur Rahman on 18/1/23.
+//
+
+import Foundation
+import CoreData
+
+struct CoreDataManager {
+    let persistentContainer: NSPersistentContainer
+    static let shared = CoreDataManager()
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "MovieAppModel")
+        persistentContainer.loadPersistentStores() { (description, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
